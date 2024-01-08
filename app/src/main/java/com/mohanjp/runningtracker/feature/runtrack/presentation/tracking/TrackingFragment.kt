73d3011115adfedbd99ecc549cc784f30c4e7c2a
Mohan <jpmohan007@gmail.com>
2024-01-08 07:44:35 +0530

    implmented map lifecycle..

--- app/src/main/java/com/mohanjp/runningtracker/feature/runtrack/presentation/tracking/TrackingFragment.kt
+++ app/src/main/java/com/mohanjp/runningtracker/feature/runtrack/presentation/tracking/TrackingFragment.kt
@@ -3,15 +3,57 @@ package com.mohanjp.runningtracker.feature.runtrack.presentation.tracking
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
 import com.mohanjp.runningtracker.R
 import com.mohanjp.runningtracker.databinding.FragmentTrackingBinding
 
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
+    private lateinit var binding: FragmentTrackingBinding
+
+    private var map: GoogleMap? = null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val binding = FragmentTrackingBinding.bind(view)
+        binding = FragmentTrackingBinding.bind(view)
         binding.mapView.onCreate(savedInstanceState)
+
+        binding.mapView.getMapAsync { map = it }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
     }
 }
---
+++ app/src/main/java/com/mohanjp/runningtracker/feature/runtrack/presentation/tracking/TrackingViewModel.kt
@@ -0,0 +1,11 @@
+package com.mohanjp.runningtracker.feature.runtrack.presentation.tracking
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class TrackingViewModel @Inject constructor(
+
+) : ViewModel() {
+}
