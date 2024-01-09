eb33fc3cebadd6e20bc41197b91c9e486adc529c
Mohan <jpmohan007@gmail.com>
2024-01-09 18:51:44 +0530

    app build.gradle.kts file restored

---
+++ app/build.gradle.kts
@@ -0,0 +1,107 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+}
+
+android {
+    namespace = "com.mohanjp.runningtracker"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.mohanjp.runningtracker"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        /*val googleMapApiKey = gradleLocalProperties(rootDir).getProperty("GOOGLE_MAP_API_KEY")
+        manifestPlaceholders["GOOGLE_MAP_API_KEY"] = googleMapApiKey*/
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    /**
+     * intuit
+     */
+    implementation("com.intuit.sdp:sdp-android:1.1.0")
+
+    /**
+     * navigation
+     */
+    val navVersion = "2.7.6"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
+
+    /**
+     * room
+     */
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    /**
+     * dagger - hilt
+     */
+    val hiltVersion = "2.47"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    kapt("androidx.hilt:hilt-compiler:1.1.0")
+
+    /**
+     * MP chart
+     */
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    /**
+     * google maps and location services
+     */
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    /**
+     * timber
+     */
+    implementation("com.jakewharton.timber:timber:5.0.1")
+
+    /**
+     * lifecycle - service
+     */
+    implementation("androidx.lifecycle:lifecycle-service:2.6.2")
+}
